72fcf891f4689f6348f9640e4b811306bd6f9ca8
Abhishekjain1997 <abhishekjain2589@gmail.com>
2024-02-29 12:14:08 +0530

    adding-premium-feature

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,89 +1,101 @@
-import React, { useState } from 'react';
-import classes from './Signup.module.css';
+import React, { useState } from "react";
+import classes from "./Signup.module.css";
+import { Link, useNavigate } from "react-router-dom";
 
 function Signup() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfimrPassword] = useState('');
-  const [error, setError]= useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfimrPassword] = useState("");
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleSignup = async (e) => {
     e.preventDefault();
 
-    if(!email || !password || !confirmPassword){
-      setError("All fields are mandatory!!");
-      return
+    if (!email || !password || !confirmPassword) {
+      setError("⚠️All fields are mandatory!!⚠️");
+      return;
     }
     if (password !== confirmPassword) {
-      setError('Passwords do not match');
+      setError("❌Passwords do not match❌");
       setPassword("");
       setConfimrPassword("");
       return;
     }
+    setLoading(true);
     try {
-    const res= await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjR0ypjDfdOXn_dk3wDjQEnYsoczmXlqs', {
-      method:'POST',
-      body: JSON.stringify({
-        email:email,
-        password:password,
-        returnSecureToken: true
-      }),
-      headers: {
-        'content-type' : 'application/json'
-      }
-    })
-
-      if(res.ok){
-        const data= await res.json()
-          //authCtx.login(data.idToken, data.email);
-          localStorage.setItem("email", data.email.replace(/[@.]/g, ""));
-          localStorage.setItem("token", data.idToken)
-          console.log(data)
-          //navigate('/');
-          console.log('User registered successfully');
+      const res = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBjR0ypjDfdOXn_dk3wDjQEnYsoczmXlqs",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: email,
+            password: password,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "content-type": "application/json",
+          },
         }
+      );
+
+      if (res.ok) {
+        setLoading(false);
+        // const data= await res.json()
+        navigate("/login");
+        console.log("User registered successfully");
+      } else {
+        const data = await res.json(); //in case the POST method fails, catch the response like this
+        if (data && data.error.message) {
+          setError("❌SignUp not successful- " + data.error.message + "❌");
+        } else {
+          setError("⚠️Some error occured!! Please try again..");
+        }
+      }
     } catch (error) {
-      console.error('Error signing up:', error);
+      console.error("❌Error signing up:", error + "❌");
     }
-    setEmail('');
+    setEmail("");
     setPassword("");
     setConfimrPassword("");
-    alert("signup done")
+    alert("signup done");
   };
 
   return (
     <>
-    <div className={classes.container}>
-      <form className={classes.signupForm} onSubmit={handleSignup}>
-        <h2>Sign Up</h2>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-        <input
-          type="password"
-          placeholder="Re-type Password"
-          value={confirmPassword}
-          onChange={(e) => setConfimrPassword(e.target.value)}
-          required
-        />
-        <p className={classes.errorMessage}>{error}</p>
-        <button type="submit">Sign Up</button>
-      </form>
-      <div className={classes.loginLink}>
-        <p>Have an account?Login</p>       
+      <div className={classes.container}>
+        <form className={classes.signupForm} onSubmit={handleSignup}>
+          <h2>Sign Up</h2>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Re-type Password"
+            value={confirmPassword}
+            onChange={(e) => setConfimrPassword(e.target.value)}
+            required
+          />
+          <p className={classes.errorMessage}>{error}</p>
+          {!loading && <button type="submit">Sign Up</button>}
+          <p className={classes.loginLink}>
+            <Link to="/login">Have an account?Login</Link>
+          </p>
+          {loading && <h2>Submitting Data...</h2>}
+        </form>
       </div>
-    </div>
     </>
   );
 }
